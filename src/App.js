102711800ea10066034585400dbd12d2468240c3
Tamim Jabr <tj222kg@student.lnu.se>
2022-03-09 11:58:37 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Exercises from "./components/Exercises";
+import Plans from "./components/Plans";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       Hej Ida
+      </header>   
+      <Routes>
+        <Route path="/" element={<Plans />} />
+        <Route path="/exercises" element={<Exercises />} />
+      </Routes>  
     </div>
   );
 }

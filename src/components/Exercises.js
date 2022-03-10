e240bee732f63b0e6f34c57b2db2a1232d0c076f
Ida Rosvall <ir222gn@student.lnu.se>
2022-03-10 14:34:59 +0100

    Display exercises for workout plans

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,11 +1,69 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { getExercisesByPlanId } from '../integrations/workout-api'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { CardActionArea } from '@mui/material'
+import { styled } from '@mui/material/styles'
+
+const ExercisesContainer = styled('div')`
+  display: flex;
+  flex-flow: row wrap;
+  gap: 1rem;
+  align-items: center;
+  justify-content: space-evenly;
+`;
 
 const Exercises = () => {
   let { id } = useParams()
-  console.log(id)
-  return <div>Exercises</div>
+  const [exercises, setExercises] = useState(null)
+
+  useEffect(() => {
+    const getExercises = async () => {
+      const response = await getExercisesByPlanId(id)
+      setExercises(response)
+    }
+
+    if (!exercises) {
+      getExercises()
+    }
+  }, [exercises])
+
+  return (
+    <>
+      {exercises && (
+        <ExercisesContainer>
+          {exercises.map((exercise) => (
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="350"
+                  image={exercise.gif_url}
+                  alt="Gif of the exercise"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {exercise.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Equipment: {exercise.equipment_type}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Body part: {exercise.body_part}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))
+          }
+        </ExercisesContainer>
+      )}
+    </>
+  )
 }
 
 export default Exercises
--- src/integrations/workout-api.js
+++ src/integrations/workout-api.js
@@ -2,11 +2,24 @@ export const getAllPlans = async () => {
   try {
     const response = await fetch(`${process.env.REACT_APP_API_URL}/plans`)
     if (response.status !== 200) {
-      throw new 'Unable to fetch plans'()
+      throw new Error('Unable to fetch plans')
     }
 
     return await response.json()
   } catch (error) {
-    throw new 'Unable to fetch plans'()
+    throw new Error('Unable to fetch plans')
+  }
+}
+
+export const getExercisesByPlanId = async (planId) => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/plans/${planId}/exercises`)
+    if (response.status !== 200) {
+      throw new Error('Unable to fetch exercises')
+    }
+
+    return await response.json()
+  } catch (error) {
+    throw new Error('Unable to fetch exercises')
   }
 }

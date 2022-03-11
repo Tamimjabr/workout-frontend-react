42ff6f45fa17fa91de5756bc009da102b499ec5d
Ida Rosvall <ir222gn@student.lnu.se>
2022-03-11 10:07:00 +0100

    Make equipment component a drawer

--- src/components/Equipments.js
+++ src/components/Equipments.js
@@ -1,57 +1,82 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import Box from '@mui/material/Box'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
+import Drawer from '@mui/material/Drawer'
+import Button from '@mui/material/Button'
 import { getAllEquipments } from '../integrations/workout-api'
-import ListSubheader from '@mui/material/ListSubheader'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import { IoBarbell } from 'react-icons/io5'
 
 const Equipments = () => {
   let { id } = useParams()
   const [equipments, setEquipments] = useState(null)
 
-  useEffect(() => {
-    const getEquipments = async () => {
-      const response = await getAllEquipments(id)
-      console.log(response)
-      setEquipments(response)
-      console.log(equipments)
-    }
+  const getEquipments = async () => {
+    const response = await getAllEquipments(id)
+    setEquipments(response)
+  }
 
+  const [state, setState] = React.useState({
+    right: false
+  })
+
+  const toggleDrawer = (anchor, open) => (event) => {
     if (!equipments) {
       getEquipments()
     }
-  }, [equipments, id, setEquipments])
 
-  return (
-    <>
-      {equipments && (
-        <List
-          sx={{
-            m: '1rem auto',
-            width: '100%',
-            maxWidth: 360,
-            bgcolor: 'background.paper',
-            '& a': { textDecoration: 'none' }
-          }}
-          subheader={
-            <ListSubheader component='div' id='Equipments'>
-              Equipments:
-            </ListSubheader>
-          }
-        >
-          {equipments.map((equipment) => (
-            <ListItem disablePadding>
-              <ListItemButton component='a' href='#simple-list'>
-                <ListItemText primary={equipment.equipment_type} />
-              </ListItemButton>
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return
+    }
+
+    setState({ ...state, [anchor]: open })
+  }
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role='presentation'
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {equipments &&
+          equipments.map((equipment, index) => (
+            <ListItem button key={index}>
+              <ListItemIcon>
+                <IoBarbell />
+              </ListItemIcon>
+              <ListItemText
+                primary={equipment.equipment_type}
+              />
             </ListItem>
           ))}
-        </List>
-      )}
-    </>
+      </List>
+    </Box>
+  )
+
+  return (
+    <div>
+      {['right'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)} variant='contained' color='info'>Equipments</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
   )
 }
 
